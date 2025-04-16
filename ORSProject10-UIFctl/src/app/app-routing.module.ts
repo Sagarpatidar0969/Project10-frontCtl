5e240fd907ac25b61b419127c6263acbc74530ad
Sagar Patidar <146437226+Sagarpatidar0969@users.noreply.github.com>
2025-04-17 02:02:18 +0530

    Update app-routing.module.ts

--- ORSProject10-UIFctl/src/app/app-routing.module.ts
+++ ORSProject10-UIFctl/src/app/app-routing.module.ts
@@ -283,7 +283,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: false })],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 
